da43e515f160874831ef52c73ab30c563a31c3ff
esraaamal <ma.esraa@hotmail.com>
2020-03-16 13:47:38 +0200

    add js file

--- js/app.js
+++ js/app.js
@@ -1,20 +1,11 @@
-`use strict`;
 
-var newArr =[1,2,3,4,5];
 
-function greaterThan(a){
-    var a =a ;
-    var count=0;
-for(var i=0 ; i< newArr.length ;i++){
+   
 
-if(newArr[i] >a){
 
-    count++ ;
-}
 
-}
-document.getElementById('showResult').innerHTML=`${count}`;
 
-}
 
-greaterThan(2);
+
+
+
