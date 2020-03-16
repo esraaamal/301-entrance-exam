3261131e3ad8c033014c2607be3c458c09845903
esraaamal <ma.esraa@hotmail.com>
2020-03-16 12:36:27 +0200

    make edit on Q1 to use return statemnent

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 `use strict`;
 
-var newArr =[1,2,3,4,5];
+var newArr =[1,2,3,4];
 
 function greaterThan(a){
     var a =a ;
     var count=0;
+    
 for(var i=0 ; i< newArr.length ;i++){
 
 if(newArr[i] >a){
@@ -13,8 +14,17 @@ if(newArr[i] >a){
 }
 
 }
+var msg =`the number of number that are greater than ${a} is:`
+return[ msg , count];
 document.getElementById('showResult').innerHTML=`${count}`;
 
 }
 
 greaterThan(2);
+console.log(greaterThan(2))
+
+
+///solve Q2 now /////////////////////////////////////////////
+
+
+
